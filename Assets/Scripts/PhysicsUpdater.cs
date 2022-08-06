04c71a739e31fa9de420c884421b9b7ad01f36b1
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-08-06 14:42:19 -0300

    feat: physics updater

---
+++ Assets/Scripts/PhysicsUpdater.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Debug = UnityEngine.Debug;
+
+public class PhysicsUpdater : IDisposable
+{
+    const float MAX_DELTA = 0.3f;
+    const float FIXED_TIME_STEP = 0.02f;
+
+    public event Action OnPreStep;
+    public event Action<double> OnStep;
+    public event Action OnPostStep;
+
+    readonly Stopwatch time = new();
+
+    Thread physicsThread;
+    bool keepRunning;
+
+    double currentTime;
+    double accumulator;
+
+    public void Initialize()
+    {
+        physicsThread = new Thread(PhysicsLoop)
+        {
+            Name = "Physics Thread"
+        };
+        physicsThread.Start();
+    }
+
+    void PhysicsLoop()
+    {
+        while (keepRunning)
+        {
+            try
+            {
+                double newTime = time.Elapsed.TotalSeconds;
+                double frameTime = newTime - currentTime;
+
+                if (frameTime > MAX_DELTA)
+                    frameTime = MAX_DELTA;
+
+                currentTime = newTime;
+
+                accumulator += frameTime;
+
+                while (accumulator >= FIXED_TIME_STEP)
+                {
+                    double deltaTime = FIXED_TIME_STEP;
+
+                    OnPreStep?.Invoke();
+                    OnStep?.Invoke(deltaTime);
+                    OnPostStep?.Invoke();
+
+                    accumulator -= deltaTime;
+                }
+
+                // float alpha = accumulator / (FIXED_TIME_STEP );
+
+                // foreach (var body in bodies)
+                // {
+                //     body.Interpolate(ref alpha);
+                // }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        keepRunning = false;
+    }
+}
